13cb020520c7298a03ae0239cea07903bfddd0b9
Bobby Rong <rjl931189261@126.com>
2024-10-16 12:14:26 +0000

    xfce.exo: 4.18.0 -> 4.19.1
    https://gitlab.xfce.org/xfce/exo/-/compare/exo-4.18.0...exo-4.19.1

--- pkgs/desktops/xfce/core/exo/default.nix
+++ pkgs/desktops/xfce/core/exo/default.nix
@@ -4,9 +4,9 @@
 mkXfceDerivation {
   category = "xfce";
   pname = "exo";
-  version = "4.18.0";
+  version = "4.19.1";
 
-  sha256 = "sha256-oWlKeUD1v2qqb8vY+2Cu9VJ1iThFPVboP12m/ob5KSQ=";
+  sha256 = "sha256-GkryGsbvkOPlRimzE/5WXWRFqOIj9/Midq6Gal0b7DE=";
 
   nativeBuildInputs = [
     libxslt
@@ -18,12 +18,8 @@ mkXfceDerivation {
     glib
     libxfce4ui
     libxfce4util
-
     (perl.withPackages(ps: with ps; [ URI ])) # for $out/lib/xfce4/exo/exo-compose-mail
   ];
-
-  # Workaround https://bugzilla.xfce.org/show_bug.cgi?id=15825
-  env.NIX_CFLAGS_COMPILE = "-I${glib.dev}/include/gio-unix-2.0";
 
   meta = with lib; {
     description = "Application library for Xfce";
