6fb15d6aca6584fd4bd688946b361d885d2dcc7f
Bobby Rong <rjl931189261@126.com>
2024-10-16 12:15:19 +0000

    xfce.xfce4-panel: 4.18.6 -> 4.19.6
    https://gitlab.xfce.org/xfce/xfce4-panel/-/compare/xfce4-panel-4.18.6...xfce4-panel-4.19.6

--- pkgs/desktops/xfce/core/xfce4-panel/default.nix
+++ pkgs/desktops/xfce/core/xfce4-panel/default.nix
@@ -1,24 +1,28 @@
 { lib
 , mkXfceDerivation
+, cairo
 , exo
 , garcon
 , gobject-introspection
+, gtk-layer-shell
 , gtk3
 , libdbusmenu-gtk3
 , libwnck
 , libxfce4ui
 , libxfce4util
+, libxfce4windowing
 , tzdata
 , vala
+, wayland
 , xfconf
 }:
 
 mkXfceDerivation {
   category = "xfce";
   pname = "xfce4-panel";
-  version = "4.18.6";
+  version = "4.19.6";
 
-  sha256 = "sha256-eQLz/LJIx2WkzcSLytRdJdhtGv0woT48mdqG7eHB0U4=";
+  sha256 = "sha256-hREVkoS4Wng5CoN4GCXooS4KkFwkX79nSzS1wh0IhTA=";
 
   nativeBuildInputs = [
     gobject-introspection
@@ -26,21 +30,23 @@ mkXfceDerivation {
   ];
 
   buildInputs = [
+    cairo
     exo
     garcon
+    gtk-layer-shell
     libdbusmenu-gtk3
     libxfce4ui
+    libxfce4windowing
     libwnck
-    xfconf
     tzdata
+    wayland
+    xfconf
   ];
 
   propagatedBuildInputs = [
     gtk3
     libxfce4util
   ];
-
-  patches = [ ./xfce4-panel-datadir.patch ];
 
   postPatch = ''
     substituteInPlace plugins/clock/clock.c \
