4eb6fdf0e780bac8d4179e6b1822a565e3b4dbc3
Bobby Rong <rjl931189261@126.com>
2024-10-16 12:14:57 +0000

    xfce.xfce4-appfinder: 4.18.1 -> 4.19.3
    https://gitlab.xfce.org/xfce/xfce4-appfinder/-/compare/xfce4-appfinder-4.18.1...xfce4-appfinder-4.19.3

--- pkgs/desktops/xfce/core/xfce4-appfinder/default.nix
+++ pkgs/desktops/xfce/core/xfce4-appfinder/default.nix
@@ -3,9 +3,9 @@
 mkXfceDerivation {
   category = "xfce";
   pname = "xfce4-appfinder";
-  version = "4.18.1";
+  version = "4.19.3";
 
-  sha256 = "sha256-CZEX1PzFsVt72Fkb4+5PiZjAcDisvYnbzFGFXjFL4sc=";
+  sha256 = "sha256-1rTD/PgS2qWBlnwZlP5Klp+fjWAHHoPHETvCT/4BvaQ=";
 
   nativeBuildInputs = [ exo ];
   buildInputs = [ garcon gtk3 libxfce4ui libxfce4util xfconf ];
